b4c47e8ed4357698a6ef4753f0f354f092aefda9
Ryan Gille <ryan@e-monsite.com>
2022-06-09 17:30:09 +0200

    🚧 i18n WIP

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -6,29 +6,20 @@
   >
   <ul class="mb-10 mt-4 lg:mt-2">
     <li class="px-2 mt-1">
-      <strong class="mr-1">Téléphone</strong>
-      <a href="#" class="block"
-        >&#048;&#054;&#046;&#055;&#050;&#046;&#052;&#057;&#046;&#055;&#051;&#046;&#050;&#050;</a
-      >
+      <strong class="mr-1">{{ $t('contact.phone.label') }}</strong>
+      <a href="#" class="block" v-html="$t('contact.phone.value')"></a>
     </li>
     <li class="px-2 mt-1">
-      <strong class="mr-1">E-mail</strong>
-      <a href="mailto:" class="block"
-        >&#103;&#105;&#108;&#108;&#101;&#115;&#046;&#114;&#121;&#097;&#110;&#046;&#100;&#101;&#118;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;</a
-      >
+      <strong class="mr-1">{{ $t('contact.email.label') }}</strong>
+      <a href="mailto:" class="block" v-html="$t('contact.email.value')"></a>
     </li>
     <li class="px-2 mt-1">
-      <strong class="mr-1">Type de contrat</strong>
-      <span class="block"
-        >CDD/CDI en full-remote ou hybride
-        <span class="text-xs">(Permis B)</span></span
-      >
+      <strong class="mr-1">{{ $t('contact.contract_type.label') }}</strong>
+      <span class="block" v-html="$t('contact.contract_type.value')"></span>
     </li>
     <li class="px-2 mt-1">
-      <strong class="mr-1">Zone de recherche</strong>
-      <span class="block"
-        >Nouvelle Aquitaine, Bordeaux, La Rochelle, Saintes</span
-      >
+      <strong class="mr-1">{{ $t('contact.search_area.label') }}</strong>
+      <span class="block" v-html="$t('contact.search_area.value')"></span>
     </li>
   </ul>
 </template>
